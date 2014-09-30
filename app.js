4650d3267b4bfa05d0053291b85a1e1957e8b2ac
E L <ekluo1@gmail.com>
2014-09-30 17:27:25 -0500

    More analysis infrastructure, options

--- app.js
+++ app.js
@@ -1,5 +1,127 @@
 var Mailman = require('./mailman');
+var fs = require('fs');
 
-Mailman.getMail(function (messages) {
-	console.log('***', messages)
-});
+// Configuration options.
+var options = {
+	fetchFromFile: true,
+	outputFile: 'my_email.txt',
+	limit: undefined,
+	myEmailAddress: 'ekluo1@gmail.com'
+};
+
+// Either fetch email data from online and save to a file, or read from a
+// previously saved file.  Both paths will end with a call to analysis.
+if (options.fetchFromFile){
+	fetchEmailFromFile(options.outputFile);
+} else {
+	fetchEmailFromServer(options.outputFile, options.limit);
+}
+
+
+
+/**
+ * Directly analyze the email data from a previously saved file.
+ * 
+ * @param  {String} outputFile The output file.
+ */
+function fetchEmailFromFile (outputFile) {
+	fs.readFile(outputFile, 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		
+		analyzeEmails(JSON.parse(data));
+	});
+}
+
+/**
+ * Uses the imap module via Mailman to fetch emails from the server.  After
+ * writing the results to a file, it attempts to analyze the emails.
+ *
+ * @param {String} outputFile 	The output file.
+ * @param {Number} limit 		The number of emails to get.  Leave undefined to
+ *                         		get all emails.
+ */
+function fetchEmailFromServer (outputFile, limit) {
+	// We can fetch emails to be analyzed and write the results to a file.
+	Mailman.getMail(function (messages){
+		// console.log(messages);
+
+		fs.writeFile(outputFile, JSON.stringify(messages), function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("The file was saved!");
+			}
+		});
+
+		analyzeEmails(messages);
+	}, limit);
+}
+
+/**
+ * Analyzes email message data.
+ * 
+ * @param  {Array.Object} messages 	A list of email metadata objects.
+ * @return {Object}          		A results object.
+ */
+function analyzeEmails (messages){
+	var messageCount = messages.length;
+	console.log('Analyzing', messages.length, 'messages...');
+
+	// Get a list of unique contacts.
+	var contactHash = {};
+	var contacts = [];
+	for (var i=0; i<messageCount; i++){
+		var message = messages[i];
+		var contact;
+
+		// Skip if I am not a sender.
+		if (containsMyAddress(message.from) === false){
+			continue;
+		}
+
+		// Skip if the to field is blank.
+		if (!message.to) continue;
+		
+		// Clean the list.
+		message.to = (message.to.length === 1) ? message.to[0].split(', ') :
+				message.to;
+		message.to = message.to.map(function (el) {
+			return el.toLowerCase();
+		}).filter(function (el){
+			return el.indexOf('@') !== -1 && el.indexOf('.') !== -1;
+		});
+		
+		// Check the to fields.
+		for (j=0; j<message.to.length; j++) {
+			contact = message.to[j];
+
+			// Hash it if we haven't seen it, excluding your own address.
+			if (contactHash[contact] !== true &&
+					contact.indexOf(options.myEmailAddress) === -1){
+				contactHash[contact] = true;
+				contacts.push(contact);
+			}
+		}
+	}
+
+	console.log(contacts, "\nFound", contacts.length, "people you sent emails to.");
+
+	// Now, what was your response time?
+}
+
+/**
+ * Returns whether or not the list of contacts contains my email address.
+ * 
+ * @param  {Array.String} contactList 	The contacts to verify.
+ * @return {boolean}             		True if the list contains my email.
+ */
+function containsMyAddress (contactList) {
+	for (var i = 0; i < contactList.length; i++) {
+		if (contactList[i].indexOf(options.myEmailAddress) !== -1){
+			return true;
+		}
+	}
+	return false;
+}
--- mailman.js
+++ mailman.js
@@ -16,7 +16,7 @@ var imap = new Imap({
 });
 
 function openInbox(cb) {
-	imap.openBox('INBOX', true, cb);
+	imap.openBox('[Gmail]/All Mail', true, cb);
 }
 
 function errlog(err){
@@ -45,37 +45,69 @@ module.exports = {
 
 	/**
 	 * Gets the mail for the user and returns a promise.
-	 * @return {Q.promise} The promise for the mail data.
 	 */
-	getMail: function (callback){
+	getMail: function (callback, count){
+		// Call the functions that were waiting for the imap to be ready.
+		imap.once('ready', function() {
+			module.exports.ready = true;
+
+			module.exports.getMail(function (){
+				for (var i = 0; i < module.exports.queuedCallbacks.length; i++) {
+					module.exports.queuedCallbacks[i](module.exports.messages);
+				}
+			});
+		});
+
 		if (!this.ready){
 			if (callback){
 				this.queuedCallbacks.push(callback);
 			}
 		} else {
+			var startTime = Date.now();
+
 			openInbox(function(err, box) {
 				if (err) throw err;
-				var f = imap.seq.fetch('1:10', {
+				var total = box.messages.total;
+
+				// Fetch X messages by default.
+				if (typeof count === 'undefined'){
+					count = total - 1;
+				}
+				count = Math.min(count, total-1);
+
+				var f = imap.seq.fetch((total-count)+':'+total, {
 					bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-					struct: true
+					struct: true,
+					size: true
 				});
 
 				var messages = [];
 				f.on('message', function(msg, seqno) {
+					// Print number to indicate progress (when fetching
+					// thousands of emails).
+					if (seqno % 1000 === 0){
+						console.log(seqno);
+					}
+
 					var buffer = '';
+					var size = 0;
 					msg.on('body', function(stream, info) {
 						stream.on('data', function(chunk) {
 							buffer += chunk.toString('utf8');
 						});
+						size += info.size;
 					});
 					msg.once('end', function() {
-						messages.push(Imap.parseHeader(buffer));
+						var msgObj = Imap.parseHeader(buffer);
+						msgObj['size'] = size;
+						messages.push(msgObj);
 					});
 				});
 
 				f.once('error', errlog);
 				f.once('end', function() {
 					console.log('Done fetching all messages!');
+					console.log('Finished in '+((Date.now() - startTime)/1000)+' s');
 
 					// Save the messages and call the callback.
 					module.exports.messages = messages;
@@ -88,14 +120,3 @@ module.exports = {
 		}
 	}
 };
-
-// Call the functions that were waiting for the imap to be ready.
-imap.once('ready', function() {
-	module.exports.ready = true;
-
-	module.exports.getMail(function (){
-		for (var i = 0; i < module.exports.queuedCallbacks.length; i++) {
-			module.exports.queuedCallbacks[i](module.exports.messages);
-		}
-	});
-});
