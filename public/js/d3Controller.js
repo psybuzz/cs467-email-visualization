99cd104a6256f87ccd613c2ad979373f9bf7e175
chrischris292 <cbchan2@illinois.edu>
2014-10-01 16:02:25 -0500

    alright.....so messy....but its done

--- public/js/d3Controller.js
+++ public/js/d3Controller.js
@@ -1,22 +1,29 @@
-var jsonDataGlobal; //holds array of values which will be taken from backend. used for legend in d3.
+var jsonDataGlobal = []; //holds array of values which will be taken from backend. used for legend in d3.
 
 
 
 $("document").ready(function(){
-  createSVG();
-  createSVG()
-  createSVG()
-  createSVG()
-  createSVG()
-  createSVG()
-
+  createSVG(.1,"hi",.3,"jennifer");
+  createSVG(.1,"hi",.4,"jack");
+  createSVG(.1,"hi",.5,"jill");
+  createSVG(.1,"hi",.2,"yugioh");
   createLegend(); //create a legend
 })
 
 
-function createSVG()
+function createSVG(index_p,text_p,value_p,name_p)
 {
       //Variable declarations for SVGs...i know its messy to put here...but aiya
+      var temp = [
+        {index: index_p, text: text_p, value: value_p ,name: name_p}
+      ];
+      jsonDataGlobal.push(temp)
+
+      var index_ = index_p;
+      var text_ = text_p;
+      var value_ = value_p;
+      var name_ = name_p;
+
       var width = 300,
       height = 300,
       radius = 600,
@@ -88,10 +95,10 @@ function createSVG()
 
     function fields() {
       var now = new Date; //just messing with values
-      jsonDataGlobal = [
-        {index: .1, text: "26 min", value: .23,name: "emily"}
+      var temp1 = [
+        {index: index_, text: text_, value: value_ ,name: name_}
       ];
-      return jsonDataGlobal;
+      return temp1;
     }
     // Avoid shortest-path interpolation.
     function interpolateHsl(a, b) {
@@ -113,16 +120,16 @@ function createSVG()
     }
 
 function createLegend(){
+  console.log(jsonDataGlobal)
   jsonDataGlobal.forEach(function(data){
-      var redColor = color(data.value).rgb().r;
-      var greenColor = color(data.value).rgb().g;
-      var blueColor = color(data.value).rgb().b;
-      var dataValue = data.value;
-      var dataText = data.text;
-      var dataName = data.name;
+      var redColor = color(data[0].value).rgb().r;
+      var greenColor = color(data[0].value).rgb().g;
+      var blueColor = color(data[0].value).rgb().b;
+      var dataValue = data[0].value;
+      var dataText = data[0].text;
+      var dataName = data[0].name;
       $("#legend-labels").append("<li id = '"+ dataName+ "'></li>");
      $("#"+dataName).text(dataName)
-     console.log($("#"+dataName +" span").text("fuck"))
      $("#"+dataName).append("<span></span>")
      var backgroundColor = " rgb(" + redColor + "," + greenColor + "," + blueColor + ")";
      $("#"+dataName+" span").css("background",backgroundColor);
