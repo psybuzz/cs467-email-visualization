68f3e59b91e25e82bf4ff2e04fb146c250d74793
E L <ekluo1@gmail.com>
2014-10-02 17:15:37 -0500

    add more frontend stats

--- public/js/ui.js
+++ public/js/ui.js
@@ -76,7 +76,7 @@ $loadDataBtn.click(function (){
 
 			// Sort contacts.
 			state.contacts = state.contacts.sort(function (a, b){
-				return state.emailCount[a] < state.emailCount[b];
+				return state.emailCount[a] - state.emailCount[b];
 			});
 
 			// Process the data.
@@ -116,15 +116,19 @@ function processLoadedData (){
 	}).filter(function (el) {
 		return el > 0;
 	}).sort(function (a, b) {
-		return a < b;
+		return a - b;
 	});
+
 	var theirResponseTimeValues = state.contacts.map(function (contact){
 		return state.avgResponseTimes[contact].theirs;
 	}).filter(function (el) {
 		return el > 0;
 	}).sort(function (a, b) {
-		return a < b;
+		return a - b;
 	});;
+
+	state.myResponseTimeValues = myResponseTimeValues;
+	state.theirResponseTimeValues = theirResponseTimeValues;
 
 	var overallMySum = myResponseTimeValues.reduce(function (a, b){return a+b});
 	var overallTheirSum = theirResponseTimeValues.reduce(function (a, b){return a+b});
@@ -148,15 +152,27 @@ function processLoadedData (){
  * Displays the data loaded into the state variable using SVG.
  */
 function displayLoadedData (){
+	var peopleCount = 50;
+
+	var cleanContacts = state.contacts.filter(function (e){
+		return state.avgResponseTimes[e].mine > 0;
+	}).sort(function (a, b){
+		return state.avgResponseTimes[a].mine - state.avgResponseTimes[b].mine;
+	}).slice(0,peopleCount);
+
+	var maxTime = Math.max.apply(null, cleanContacts.map(function (el){
+		return state.avgResponseTimes[el].mine;
+	}));
+
 	// Create a bubble for each person.
-	for (i = 0; i < state.contacts.length; i++) {
-		contact = state.contacts[i];
+	for (i = 0; i < Math.min(peopleCount, cleanContacts.length); i++) {
+		contact = cleanContacts[i];
 		myAvgTime = state.avgResponseTimes[contact].mine;
 		theirAvgTime = state.avgResponseTimes[contact].theirs;
 
 		if (myAvgTime > 0){
 			// Make a circle for my average response time with this person.
-			createSVG(.1, prettyTime(myAvgTime), myAvgTime / state.maxMyAvgTime, contact);
+			createSVG(.1, prettyTime(myAvgTime), myAvgTime / maxTime, contact);
 		}
 	}
 
@@ -176,6 +192,46 @@ function displayLoadedData (){
 			prettyTime(state.minTheirAvgTime) +
 			" to " +
 			prettyTime(state.maxTheirAvgTime));
+
+	var mineContactSorted = state.contacts.filter(function (el) {
+		return state.avgResponseTimes[el].mine > 0;
+	}).sort(function (a, b) {
+		return state.avgResponseTimes[a].mine - state.avgResponseTimes[b].mine;
+	});
+
+	var theirsContactSorted = state.contacts.filter(function (el) {
+		return state.avgResponseTimes[el].theirs > 0;
+	}).sort(function (a, b) {
+		return state.avgResponseTimes[a].theirs - state.avgResponseTimes[b].theirs;
+	});
+
+	$('#mine-fast').html(
+			"<li>" + 
+			mineContactSorted.slice(0, 10).map(function (el){
+				return prettyTime(state.avgResponseTimes[el].mine) + ": " + el;
+			}).join('</li><li>') +
+			"</li>");
+
+	$('#mine-slow').html(
+			"<li>" + 
+			mineContactSorted.slice(mineContactSorted.length - 10, mineContactSorted.length).map(function (el){
+				return prettyTime(state.avgResponseTimes[el].mine) + ": " + el;
+			}).join('</li><li>') +
+			"</li>");
+
+	$('#theirs-slow').html(
+			"<li>" + 
+			theirsContactSorted.slice(theirsContactSorted.length - 10, theirsContactSorted.length).map(function (el){
+				return prettyTime(state.avgResponseTimes[el].theirs) + ": " + el;
+			}).join('</li><li>') +
+			"</li>");
+
+	$('#theirs-fast').html(
+			"<li>" + 
+			theirsContactSorted.slice(0, 10).map(function (el){
+				return prettyTime(state.avgResponseTimes[el].theirs) + ": " + el;
+			}).join('</li><li>') +
+			"</li>");
 }
 
 // Utility functions.
