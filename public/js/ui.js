16d6829126e88327ad8d37fe217da336fb2622ea
E L <ekluo1@gmail.com>
2014-10-06 00:59:09 -0500

    Add details to fake data

--- public/js/ui.js
+++ public/js/ui.js
@@ -61,10 +61,10 @@ $loadFakeBtn = $('#load-fake-btn');
 $loadFakeBtn.click(function (){
 	resetSvgContainer();
 
-	createSVG(.1,"30 min",.3,"January");
-	createSVG(.1,"40 min",.4,"Febuary");
-	createSVG(.1,"50 min",.5,"March");
-	createSVG(.1,"90 min",.9,"April");
+	createSVG(.1,"30 min",.3,"January", 'monthContainer');
+	createSVG(.1,"40 min",.4,"Febuary", 'monthContainer');
+	createSVG(.1,"50 min",.5,"March", 'monthContainer');
+	createSVG(.1,"90 min",.9,"April", 'monthContainer');
 	
 	//demo of ticking
 	createSVG2(.1,"30 min",.3,"50 min",.5,"90 min",.9,"10 min",.1, "25 min",.25,"FriendA");
@@ -72,8 +72,24 @@ $loadFakeBtn.click(function (){
 	createSVG2(.1,"90 min",.9,"70 min",.7,"50 min",.5,"30 min",.3, "10 min",.1,"FriendC");
 	createSVG2(.1,"10 min",.1,"30 min",.3,"50 min",.5,"70 min",.7, "90 min",.9,"FriendD");
 	
-	// Display the loaded data.
-	displayLoadedData();
+	$('#big-stats').html('Your average response time is <span class="fast">34 days</span> (median: 2 days) ranging from 1 min to 879 days.<br>It takes others about <span class="slow">17 days</span> (median: 3 days) to respond to you ranging from 23 sec to 443 days');
+
+	$('#top-list').html('' +
+			'<div class="col">' +
+				'<span class="list-title">People I respond <span class="fast">quickly</span> to</span>' +
+				'<ul id="mine-fast"><li>1 min: jonny.x@linkedin.com</li><li>4 min: adeline@gmail.com</li><li>5 min: prc@uiuc.edu</li><li>7 min: norman.c@uiuc.edu</li><li>7 min: maks@uiuc.edu</li></ul>' +
+				'<span class="list-title">People who respond <span class="fast">quickly</span> to me</span>' +
+				'<ul id="theirs-fast"><li>23 sec: jobs@google.com</li><li>25 sec: it-help@illinois.edu</li><li>1 min: mrstuart@linkedin.com</li><li>1 min: ipod@illinois.edu</li><li>2 min: dragonista@gmail.com</li></ul>' +
+			'</div>' +
+			'<div class="col">' +
+				'<span class="list-title">People I respond <span class="slow">slowly</span> to</span>' +
+				'<ul id="mine-slow"><li>879 days: whoami@gmail.com</li><li>428 days: wongfu@gmail.com</li><li>395 days: curryandpasta@gmail.com</li><li>381 days: meet1232132@gmail.com</li><li>318 days: pmonrose@illinois.edu</li></ul>' +
+				'<span class="list-title">People who respond <span class="slow">slowly</span> to me</span>' +
+				'<ul id="theirs-slow"><li>443 days: mystery@uiuc.edu</li><li>196 days: joe.doe@gmail.com</li><li>187 days: lorem.ipsum2@gmail.com</li><li>159 days: adacraft332@gmail.com</li><li>116 days: jamescp@gmail.com</li></ul>' +
+			'</div>');
+	
+
+	$('#results').show();
 });
 
 $loadDataBtn.click(function (){
